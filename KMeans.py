0d9583751236a3dd556fc21dcd25430336322f1d
Ricardo Moya <rmoya@logtrust.com>
2016-03-14 20:26:10 +0100

    cambio del path de los data set

--- KMeans.py
+++ KMeans.py
@@ -8,10 +8,10 @@ from scipy.spatial import distance
 from Point import Point
 from Cluster import Cluster
 
-DATASET1 = "../dataSet/DSclustering/DS_3Clusters_999Points.txt"
-DATASET2 = "../dataSet/DSclustering/DS2_3Clusters_999Points.txt"
-DATASET3 = "../dataSet/DSclustering/DS_5Clusters_10000Points.txt"
-DATASET4 = "../dataSet/DSclustering/DS_7Clusters_100000Points.txt"
+DATASET1 = "./dataSet/DS_3Clusters_999Points.txt"
+DATASET2 = "./dataSet/DS2_3Clusters_999Points.txt"
+DATASET3 = "./dataSet/DS_5Clusters_10000Points.txt"
+DATASET4 = "./dataSet/DS_7Clusters_100000Points.txt"
 NUM_CLUSTERS = 3
 ITERATIONS = 1000
 COLORS = ['red', 'blue', 'green', 'yellow', 'gray', 'pink', 'violet', 'brown',
--- KMeans_scikit.py
+++ KMeans_scikit.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
 # Constant
-DATASET1 = "../dataSet/DSclustering/DS_3Clusters_999Points.txt"
-DATASET2 = "../dataSet/DSclustering/DS2_3Clusters_999Points.txt"
-DATASET3 = "../dataSet/DSclustering/DS_5Clusters_10000Points.txt"
-DATASET4 = "../dataSet/DSclustering/DS_7Clusters_100000Points.txt"
+DATASET1 = "./dataSet/DS_3Clusters_999Points.txt"
+DATASET2 = "./dataSet/DS2_3Clusters_999Points.txt"
+DATASET3 = "./dataSet/DS_5Clusters_10000Points.txt"
+DATASET4 = "./dataSet/DS_7Clusters_100000Points.txt"
 NUM_CLUSTERS = 3
 MAX_ITERATIONS = 10
 INITIALIZE_CLUSTERS = ['k-means++', 'random']
